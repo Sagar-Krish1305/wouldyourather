ad28f74f4f7de29bb43607024d35bcb63fbabce9
Devam <satasiyadevam@gmail.com>
2024-05-30 19:07:50 +0530

    Final Commit

---
+++ wouldyourather/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect  ,useState } from "react";
+
+import {MainPage}  from "./Krish/Pages/Main Page/MainPage";
+
+function App() {
+
+  return (
+    
+    <div>
+      <MainPage  />
+    </div>
+);
+}
+
+export default App;
+
---
+++ wouldyourather/src/config/config.js
@@ -0,0 +1,4 @@
+export const config  = {
+    xrapidapikey : String(import.meta.env.VITE_XRAPITAPI_KEY),
+    xrapidhost  : String (import.meta.env.VITE_XRAPIDAPI_HOST)
+}
