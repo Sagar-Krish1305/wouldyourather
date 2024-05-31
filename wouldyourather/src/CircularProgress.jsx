dff1345f12af58d76a54dd7a09cbd9ecc468f28f
Devam <satasiyadevam@gmail.com>
2024-05-31 17:28:18 +0530

    manage api and circular bar

---
+++ wouldyourather/src/CircularProgress.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import './CircularProgress.css';
+
+const CircularProgress = ({ value }) => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let currentPercentage = 0;
+    const stepTime = 20; // milliseconds
+
+    function updateProgress() {
+      if (currentPercentage < value) {
+        currentPercentage++;
+      } else if (currentPercentage > value) {
+        currentPercentage--;
+      }
+
+      setProgress(currentPercentage);
+
+      if (currentPercentage !== value) {
+        setTimeout(updateProgress, stepTime);
+      }
+    }
+
+    updateProgress();
+  }, [value]);
+
+  const angle = progress * 3.6;
+
+  return (
+    <div className="progress-container">
+      <div
+        className="progress-circle"
+        style={{ background: `conic-gradient(#598ef0 ${angle}deg,#f15361 ${angle}deg)` }}
+      >
+     
+      </div>
+    </div>
+  );
+};
+
+export default CircularProgress;
